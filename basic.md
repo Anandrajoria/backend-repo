eabcdcdded189dd18ff8210316ca3a9671aaff5e
Anandrajoriya <anandrajoriya03@gmail.com>
2025-07-05 19:04:09 +0530

    backend basice

---
+++ basic.md
@@ -0,0 +1,9 @@
+programming language X
+technology X
+framework X
+library X
+
+
+
+what actually is =>node js is javascript run time environment
+
---
+++ index.js
@@ -0,0 +1,59 @@
+//! get most of fs practice
+
+const fs = require("fs");
+
+
+//! write file
+
+// fs.writeFile("hey.txt", "hey hello world", (err) => {
+//   if (err) console.log(err);
+//   else console.log("done");
+// });
+
+//! append file
+
+// fs.appendFile("hey.txt", " you are best", (err) => {
+//   if (err) console.log(err);
+//   else console.log("done");
+// });
+
+//! rename file
+
+// fs.rename("hey.txt", "greetingFile.txt", (err) => {
+//   if (err) console.log(err);
+//   else console.log("done");
+// });
+
+//! copy file
+// fs.copyFile("greetingFile.txt", "./copy2/duplicate.txt", (err) => {
+//   if (err) console.log(err.message);
+//   else console.log("done");
+// });
+
+//! delete empty folder
+// fs.unlink("greetingFile.txt", (err) => {
+//   if (err) console.log(err.message);
+//   else console.log("removed");
+// });
+
+// fs.rm("./copy", (err) => {  //! it does not remove the directory even with the file inside it
+//   if (err) console.log(err.message);
+//   else console.log("removed");
+// });
+
+// ! delete non empty folder
+// fs.rm("./copy",{recursive:true}, (err) => {  //! it remove the directory even with the file inside it
+//   if (err) console.log(err.message);
+//   else console.log("removed");
+// });
+
+
+//! creating server
+// const http = require("http");
+// const server = http.createServer((req, res) => {
+//     res.end("hello world")
+// });
+
+// server.listen(3000)
+
+
